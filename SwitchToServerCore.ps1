a560bb44b03b6cb7caeb359dc5948fddc9fb5db9
ethangillani <ethan.gillani@mtusa.com>
2017-07-18 10:58:02 -0500

    Added Script to Switch Windows Server to Server Core
    The notes below the actual code will tell you how to switch back.

---
+++ SwitchToServerCore.ps1
@@ -0,0 +1,15 @@
+﻿#This script switches a server from Full Gui to Server Core
+Import-Module ServerManager
+Uninstall-Windowsfeature Server-Gui-Mgmt-Infra, Server-Gui-Shell  -Restart
+
+#If you want graphical management infrastructure and not full server core, then you run 
+#Uninstall-WindowsFeature Server-Gui-Shell 
+#and you leave the server-gui-mgmt-infra
+#
+#
+#
+#
+#To switch back to full server you run 
+# Import-Module ServerManager
+# Install-WindowsFeature Server-Gui-Mgmt-Infra, Server-Gui-Shell -Restart
+#
